0b1d54d5690cb0bf6b9aaf81b6c10206e0353ed0
Marshall Williams <marshall@mwthink.net>
2014-11-13 00:32:41 -0600

    Added report page database functions - No error reporting

--- control/public/report.php
+++ control/public/report.php
@@ -1,12 +1,17 @@
 <?php
-	require_once('../admin/droppoint.php');
-	// TODO : This page is broken. Fix it. 
-	if(!isset($_GET['code']) and !isset($_GET['domain'])){
+	require_once('../admin/droppoint.php'); 
+	if(!isset($_GET['code']) or !isset($_GET['domain'])){
 		echo "5";
 		exit();
 	}
 	if($_GET['code'] == $secretCode){
-		// TODO : Save info here - Need database
+		if($db->has("hosts",["domain"=>$_GET['domain']])){
+			$db->update("hosts",["checkInTime"=>time()],["domain"=>$_GET['domain']]);
+		}
+		else{
+			$db->insert("hosts",["domain"=>$_GET['domain'],"checkInTime"=>time()]);
+		}
+		// TODO: Error checking
 		echo "1";
 	}
 	else{
