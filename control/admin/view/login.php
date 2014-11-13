782ac2e6d9db3af23d1d64097c1a17d08543228f
Marshall Williams <marshall@mwthink.net>
2014-11-12 22:04:44 -0600

    Added in database user query for login

--- control/admin/view/login.php
+++ control/admin/view/login.php
@@ -1,12 +1,10 @@
 <?php
 	if(isset($_GET['login'])){
-		
-		// TODO: Change password storage method
-		$hash = '$2y$10$.g9ymrdNEj/EUPxBpNIa2ec0ZK4CqgLIVaLVCryz3dbE/q0.4zG.i'; // "password"
-		
-		if(password_verify($_POST['password'], $hash) and $_POST['username']=='user'){
-			$_SESSION['user']="user";
-			header("Location: index.php");
+		if($db->has("users",["username" => strtolower($_POST['username'])])){
+			if(password_verify($_POST['password'], $db->get('users','password',['username'=>strtolower($_POST['username'])]))){
+				$_SESSION['user']="user";
+				header("Location: index.php");
+			}
 		}
 	}
 ?>
