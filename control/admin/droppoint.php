64de7aa0afea9a623158cd63c4e41d92224c66ba
Marshall Williams <marshall@mwthink.net>
2014-11-12 21:51:32 -0600

    Added medoo database framework

--- control/admin/droppoint.php
+++ control/admin/droppoint.php
@@ -4,4 +4,8 @@
 	
 	defined("TEMPLATES_PATH") or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/view'));
 	defined("LIBRARY_PATH") or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/lib'));
+	
+	include(LIBRARY_PATH .'/medoo.min.php');
+	$db = new medoo(LIBRARY_PATH .'/database.db');
+	
 ?>
