7770708ed601972e35c0fcb5cee0e6d9addd3967
Marshall Williams <marshall@mwthink.net>
2014-11-12 23:31:50 -0600

    Cleaned up controls a bit. Going to bed, it is late

--- control/admin/view/controls.php
+++ control/admin/view/controls.php
@@ -2,9 +2,23 @@
 <html>
 	<head>
 		<title>DropPoint Control Panel</title>
+		<style>
+			body{
+				background-color:#94A499;
+			}
+			#wrapper{
+				width:900px;
+				background-color:#7A7A7A;
+				padding:5px 5px 5px 5px;
+				margin:0 auto;
+			}
+		</style>
 	</head>
 	<body>
-		<p>You are logged in</p>
-		<a href="index.php?logout"><button>Logout</button></a>
+		<div id="wrapper">
+			<p>You are logged in</p>
+			<a href="index.php?logout"><button>Logout</button></a>
+		</div>
+		
 	</body>
 </html>
