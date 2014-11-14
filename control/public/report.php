fb024e54206d5489d063ccd1207dd5da85c7d28e
Marshall Williams <marshall@mwthink.net>
2014-11-14 13:46:30 -0600

    More accurate report messages to client

--- control/public/report.php
+++ control/public/report.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('../admin/droppoint.php'); 
+	require_once('../admin/droppoint.php');
 	if(!isset($_GET['code']) or !isset($_GET['domain'])){
 		echo "5";
 		exit();
@@ -7,15 +7,17 @@
 	if($_GET['code'] == $secretCode){
 		if($db->has("hosts",["domain"=>$_GET['domain']])){
 			$db->update("hosts",["checkInTime"=>time()],["domain"=>$_GET['domain']]);
+			echo "2";
 		}
 		else{
 			$db->insert("hosts",["domain"=>$_GET['domain'],"checkInTime"=>time()]);
+			echo "1";
 		}
 		// TODO: Error checking
-		echo "1";
+		
 	}
 	else{
-		echo "2";
+		echo "3";
 	}
 	
 ?>
