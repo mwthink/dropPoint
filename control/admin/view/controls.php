eef0e56ab4f258103a1aa518b828cfd347cbf381
Marshall Williams <marshall@mwthink.net>
2014-11-14 16:27:51 -0600

    Cleaned up control page a bit, added Pure CSS framework and now display host list

--- control/admin/view/controls.php
+++ control/admin/view/controls.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>DropPoint Control Panel</title>
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
 		<style>
 			body{
 				background-color:#94A499;
@@ -16,8 +17,30 @@
 	</head>
 	<body>
 		<div id="wrapper">
-			<p>You are logged in</p>
-			<a href="index.php?logout"><button>Logout</button></a>
+			<div class="pure-g">
+				<div class="pure-u-1-3">
+					<p>You are logged in</p>
+					<a href="index.php?logout"><button>Logout</button></a>
+				</div>
+				<div class="pure-u-2-3">
+					<table class="pure-table" style="text-align:center;margin:0 auto;">
+						<thead>
+							<tr>
+								<th>Host Name</th>
+								<th>Time Reported</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								$hosts=$db->select("hosts",["domain","checkInTime"]);
+								foreach($hosts as $host){
+									echo "<tr><td>".$host['domain']."</td><td>".date('n-d-Y g:i A',$host['checkInTime'])."</td></tr>";
+								}
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</div>
 		
 	</body>
