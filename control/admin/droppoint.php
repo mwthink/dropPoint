96d1f59c9f21fdb598a78d473dfc8c1003aacd73
Marshall Williams <marshall@mwthink.net>
2014-11-14 17:05:08 -0600

    Moved secretKey variable to database instead of hard coded

--- control/admin/droppoint.php
+++ control/admin/droppoint.php
@@ -1,11 +1,8 @@
 <?php
-	// DropPoint Config File
-	$secretCode="changeThisCode";
-	
 	defined("TEMPLATES_PATH") or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/view'));
 	defined("LIBRARY_PATH") or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/lib'));
 	
 	include(LIBRARY_PATH .'/medoo.min.php');
 	$db = new medoo(LIBRARY_PATH .'/database.db');
-	
+	$secretCode=$db->get("settings","value",["setting"=>"secretKey"]);
 ?>
