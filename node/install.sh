b7f923e180c1168b5232d4b31acdfdbf2d3117d3
Marshall Williams <marshall@mwthink.net>
2014-11-14 12:19:41 -0600

    Added creation of hidden service directory, change owners and output hostname Owner currently only chowns to debian-tor, should look for multi-platform solutions

--- node/install.sh
+++ node/install.sh
@@ -2,10 +2,12 @@
 
 # DropPoint Installation Script
 # Tested for Rasbian on Raspberry Pi Model B
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+
 command -v tor >/dev/null 2>&1 || { echo >&2 "Installation of 'tor' not found. Exiting"; exit 1; }
 
 x=$(tor --verify-config | grep 'Configuration was valid')
@@ -15,4 +17,23 @@ fi
 x=$(cat /etc/tor/torrc | grep -v '^#' | grep 'HiddenServiceDir' | grep 'droppoint')
 if [[ $x == $(grep '' /dev/null) ]];then
 	echo "No DropPoint detected"
+	echo "Creating DropPoint"
+	mkdir /var/lib/tor/droppoint
+	echo "HiddenServiceDir /var/lib/tor/droppoint/" >> /etc/tor/torrc
+	echo "HiddenServicePort 80 127.0.0.1:80" >> /etc/tor/torrc
+	chown debian-tor:debian-tor /var/lib/tor/droppoint
+	echo "DropPoint created"
+	echo "Restarting tor service"
+	service tor restart
+	
+else
+	echo "DropPoint found"
 fi
+
+if [ ! -f /var/lib/tor/droppoint/hostname ]
+then
+    echo "FATAL - The DropPoint does not seem to have a hostname"
+    echo "Is tor running and does it own /var/lib/tor/droppoint?"
+    exit 1
+fi
+tput setaf 2; echo "Your DropPoint hostname is :" $(cat /var/lib/tor/droppoint/hostname);tput sgr0
